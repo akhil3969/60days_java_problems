a0a3747a3af60d466ab605a4e2399e10fcf0777b
Kona  sai Akhil <109036081+akhil3969@users.noreply.github.com>
2025-04-24 08:51:06 +0530

    Add files via upload

---
+++ Day2_number_reverse.java
@@ -0,0 +1,19 @@
+package days60problemsloving;
+import java.util.Scanner;
+public class Day2_number_reverse {
+  public static void main(String[] args) {
+	Scanner ob=new Scanner(System.in);
+	System.out.println("enter the number");
+	int a=ob.nextInt();
+	
+	int sum=0;
+	while(a>0) {
+	 
+		int rem=a%10;
+		sum=sum*10+rem;
+		a=a/10;
+
+	}
+	System.out.println(sum);
+}
+  }
