b4a60359cf22c4dae4b9b20ac7ffeb2f40990e9c
Kona  sai Akhil <109036081+akhil3969@users.noreply.github.com>
2025-04-25 14:22:47 +0530

    Update day1_fibnoic.java

--- day1_fibnoic.java
+++ day1_fibnoic.java
@@ -19,17 +19,3 @@ public class day1_fibnoic {
 		}
 }
 
-//    public static void main(String[] args) {
-//        int n = 10; // Number of terms
-//        int a = 0, b = 1;
-//
-//        System.out.print("Fibonacci Series: " + a + " " + b);
-//
-//        for (int i = 2; i < n; i++) {
-//            int next = a + b;
-//            System.out.print(" " + next);
-//            a = b;
-//            b = next;
-//        }
-//    }
-//}
